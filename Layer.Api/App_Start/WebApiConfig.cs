a6e6baaa707ea5d09c14ebad9fa8440b3bb756c0
Sang Thach <sang.thach@orientsoftware.net>
2017-03-21 16:46:17 +0700

    Updated Elmah

--- Layer.Api/App_Start/WebApiConfig.cs
+++ Layer.Api/App_Start/WebApiConfig.cs
@@ -1,5 +1,6 @@
-﻿using Microsoft.Owin.Security.OAuth;
+﻿using Elmah.Contrib.WebApi;
 using System.Web.Http;
+using System.Web.Http.ExceptionHandling;
 
 namespace Layer.Api
 {
@@ -12,16 +13,27 @@ namespace Layer.Api
             //config.SuppressDefaultHostAuthentication();
             //config.Filters.Add(new HostAuthenticationFilter(OAuthDefaults.AuthenticationType));
 
+            config.Services.Add(typeof(IExceptionLogger), new ElmahExceptionLogger());
+
             log4net.Config.XmlConfigurator.Configure();
 
             // Web API routes
             config.MapHttpAttributeRoutes();
+
+            // catch all route mapped to ErrorController so 404 errors
+            // can be logged in elmah
+            config.Routes.MapHttpRoute(
+                name: "NotFound",
+                routeTemplate: "{*path}",
+                defaults: new { controller = "Error", action = "NotFound" }
+            );
 
             config.Routes.MapHttpRoute(
                 name: "DefaultApi",
                 routeTemplate: "api/{controller}/{id}",
                 defaults: new { id = RouteParameter.Optional }
             );
+
         }
     }
 }
