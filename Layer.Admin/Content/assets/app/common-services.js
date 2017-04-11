cc884b701db78177be0badfece1fe7a25e4ceeff
Sang Thach <sang.thach@orientsoftware.net>
2017-04-11 15:48:22 +0700

    Middle popup in screen (have scroll)

--- Layer.Admin/Content/assets/app/common-services.js
+++ Layer.Admin/Content/assets/app/common-services.js
@@ -1,4 +1,6 @@
 ﻿var commonServices = (function () {
+    var outstandingForOverlay = 40;
+
     return {
         showModal: showModal,
         replaceHTMLOnPopup: replaceHTMLOnPopup,
@@ -6,12 +8,18 @@
     };
 
     function showModal() {
+        //set height for overlay
         var el = document.getElementById("overlay");
-        el.style.visibility = (el.style.visibility == "visible") ? "hidden" : "visible";
+        var height = $('#wrap').outerHeight() + $('#footer').outerHeight() + outstandingForOverlay;
+        $('#overlay').css({ 'visibility': 'visible', 'height': height + 'px' });
+
+        //middle for window
+        var topAfterScroll = $('body').scrollTop() + outstandingForOverlay;
+        $('#overlay .popup-container').css({ 'marginTop': topAfterScroll });
     }
 
     function replaceHTMLOnPopup(selector) {
-        var html = $(selector);
+        var html = $(selector).html();
         $('#overlay .popup-container').html(html);
     }
 
